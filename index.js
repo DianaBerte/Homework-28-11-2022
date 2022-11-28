33f29419575661aee339f2d4aa6fe20f7447ecb4
DianaBerte <dianaberte.go@gmail.com>
2022-11-28 16:11:29 +0100

    Creating index.js and index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let firstName = "Diana ";
+let lastName = "Berte";
+let wholeName = firstName + lastName;
+
+console.log("Hello, my name is", wholeName);
+
+let age = "28";
+let currentYear = "2022";
+let birthYear = currentYear - age;
+
+console.log(birthYear)
